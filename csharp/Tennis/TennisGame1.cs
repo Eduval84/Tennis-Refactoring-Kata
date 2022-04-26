34b7f9823e61f3fc065528dbd3d81dca5b12d4f0
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-26 23:34:01 +0100

    :Recycle: Extract score methods

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,71 +2,98 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int _player1Score;
-        private int _player2Score;
-        private string _score = "";
+        private int player1Score;
+        private int player2Score;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                _player1Score += 1;
+                player1Score += 1;
             else
-                _player2Score += 1;
+                player2Score += 1;
         }
 
         public string GetScore()
         {
-            if (_player1Score == _player2Score)
+            string score = "";
+            if (player1Score == player2Score)
             {
-                switch (_player1Score)
-                {
-                    case 0:
-                        _score = "Love-All";
-                        break;
-                    case 1:
-                        _score = "Fifteen-All";
-                        break;
-                    case 2:
-                        _score = "Thirty-All";
-                        break;
-                    default:
-                        _score = "Deuce";
-                        break;
-                }
+                score = EqualScoreHandle();
             }
-            else if (_player1Score >= 4 || _player2Score >= 4)
+            else if (player1Score >= 4 || player2Score >= 4)
             {
-                var minusResult = _player1Score - _player2Score;
-                if (minusResult == 1) _score = "Advantage player1";
-                else if (minusResult == -1) _score = "Advantage player2";
-                else if (minusResult >= 2) _score = "Win for player1";
-                else _score = "Win for player2";
+                score = SetAdvantageOrWinGame();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                score = SetGeneralPointsScore(score);
+            }
+            return score;
+        }
+
+        private string SetGeneralPointsScore(string score)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                int tempScore;
+                if (i == 1) tempScore = player1Score;
+                else
                 {
-                    int tempScore;
-                    if (i == 1) tempScore = _player1Score;
-                    else { _score += "-"; tempScore = _player2Score; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            _score += "Love";
-                            break;
-                        case 1:
-                            _score += "Fifteen";
-                            break;
-                        case 2:
-                            _score += "Thirty";
-                            break;
-                        case 3:
-                            _score += "Forty";
-                            break;
-                    }
+                    score += "-";
+                    tempScore = player2Score;
+                }
+
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
-            return _score;
+
+            return score;
+        }
+
+        private string SetAdvantageOrWinGame()
+        {
+            string score;
+            var minusResult = player1Score - player2Score;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private string EqualScoreHandle()
+        {
+            string score;
+            switch (player1Score)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
+            return score;
         }
     }
 }
