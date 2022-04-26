d9dcc0287500c54310825f04d25756243ed65f46
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-26 23:03:23 +0100

    :recycle: Extract score to a field

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,72 +2,71 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int player1Score;
-        private int player2Score;
+        private int _player1Score;
+        private int _player2Score;
+        private string _score = "";
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                player1Score += 1;
+                _player1Score += 1;
             else
-                player2Score += 1;
+                _player2Score += 1;
         }
 
         public string GetScore()
         {
-            string score = "";
-            int tempScore;
-            if (player1Score == player2Score)
+            if (_player1Score == _player2Score)
             {
-                switch (player1Score)
+                switch (_player1Score)
                 {
                     case 0:
-                        score = "Love-All";
+                        _score = "Love-All";
                         break;
                     case 1:
-                        score = "Fifteen-All";
+                        _score = "Fifteen-All";
                         break;
                     case 2:
-                        score = "Thirty-All";
+                        _score = "Thirty-All";
                         break;
                     default:
-                        score = "Deuce";
+                        _score = "Deuce";
                         break;
-
                 }
             }
-            else if (player1Score >= 4 || player2Score >= 4)
+            else if (_player1Score >= 4 || _player2Score >= 4)
             {
-                var minusResult = player1Score - player2Score;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                var minusResult = _player1Score - _player2Score;
+                if (minusResult == 1) _score = "Advantage player1";
+                else if (minusResult == -1) _score = "Advantage player2";
+                else if (minusResult >= 2) _score = "Win for player1";
+                else _score = "Win for player2";
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = player1Score;
-                    else { score += "-"; tempScore = player2Score; }
+                    int tempScore;
+                    if (i == 1) tempScore = _player1Score;
+                    else { _score += "-"; tempScore = _player2Score; }
                     switch (tempScore)
                     {
                         case 0:
-                            score += "Love";
+                            _score += "Love";
                             break;
                         case 1:
-                            score += "Fifteen";
+                            _score += "Fifteen";
                             break;
                         case 2:
-                            score += "Thirty";
+                            _score += "Thirty";
                             break;
                         case 3:
-                            score += "Forty";
+                            _score += "Forty";
                             break;
                     }
                 }
             }
-            return score;
+            return _score;
         }
     }
 }
