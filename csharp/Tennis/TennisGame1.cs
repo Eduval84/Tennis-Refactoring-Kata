924d22c64df836385828e8a63c67f9a590d185df
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-27 07:53:21 +0100

    ♻ refactor equalScoreHandle , inline variable and refactor switch

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -15,28 +15,27 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
             if (player1Score == player2Score)
             {
-                score = EqualScoreHandle();
+                return EqualScoreHandle();
             }
-            else if (player1Score >= 4 || player2Score >= 4)
+
+            if (player1Score >= 4 || player2Score >= 4)
             {
-                score = SetAdvantageOrWinGame();
+                return SetAdvantageOrWinGame();
             }
-            else
-            {
-                score = SetGeneralPointsScore(score);
-            }
-            return score;
+
+            return SetGeneralPointsScore();
         }
 
-        private string SetGeneralPointsScore(string score)
+        private string SetGeneralPointsScore()
         {
+            var score = "";
             for (var i = 1; i < 3; i++)
             {
                 int tempScore;
-                if (i == 1) tempScore = player1Score;
+                if (i == 1)
+                    tempScore = player1Score;
                 else
                 {
                     score += "-";
@@ -65,35 +64,24 @@ namespace Tennis
 
         private string SetAdvantageOrWinGame()
         {
-            string score;
-            var minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-            return score;
+            return (player1Score - player2Score) switch
+            {
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2"
+            };
         }
 
         private string EqualScoreHandle()
         {
-            string score;
-            switch (player1Score)
+            return player1Score switch
             {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
-
-            return score;
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce"
+            };
         }
     }
 }
