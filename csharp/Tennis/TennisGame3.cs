db970305a0aca472c689a5c350dd5f58471072f6
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-29 15:12:12 +0100

    :recycle:

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,10 +2,12 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
+        private int _player2Score;
+        private int _player1Score;
         private readonly string _player1Name;
         private readonly string _player2Name;
+        private readonly string[] _playWords = { "Love", "Fifteen", "Thirty", "Forty" };
+        private string _score;
 
         public TennisGame3(string player1Name, string player2Name)
         {
@@ -15,28 +17,35 @@ namespace Tennis
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            if (CheckIfPlayersScoreIsLessThan4AndTheSumOfAllAreLessThan6())
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                _score = _playWords[_player1Score];
+                return (_player1Score == _player2Score) ? _score + "-All" : _score + "-" + _playWords[_player2Score];
             }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? _player1Name : _player2Name;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+
+            if (CheckIfPlayer1AndPlayer2ScoresAreEqual())
+                return "Deuce";
+
+            _score = _player1Score > _player2Score ? _player1Name : _player2Name;
+            return ((_player1Score - _player2Score) * (_player1Score - _player2Score) == 1) ? "Advantage " + _score : "Win for " + _score;
+        }
+
+        private bool CheckIfPlayer1AndPlayer2ScoresAreEqual()
+        {
+            return _player1Score == _player2Score;
+        }
+
+        private bool CheckIfPlayersScoreIsLessThan4AndTheSumOfAllAreLessThan6()
+        {
+            return _player1Score < 4 && _player2Score < 4 && _player1Score + _player2Score < 6;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                _player1Score += 1;
             else
-                this.p2 += 1;
+                _player2Score += 1;
         }
 
     }
